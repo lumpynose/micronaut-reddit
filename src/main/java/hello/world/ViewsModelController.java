401527fd9a13e10ca0db33040c9d6a11a902180c
Rusty <rusty.wright@gmail.com>
2025-02-02 10:28:42 -0800

    latest

--- src/main/java/hello/world/ViewsModelController.java
+++ src/main/java/hello/world/ViewsModelController.java
@@ -5,12 +5,18 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.views.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/viewsmodel")
 class ViewsModelController {
-    @View("home.html")
+    private static final Logger log
+            = LoggerFactory.getLogger(ViewsModelController.class);
+
+    @View("home")
     @Get("/")
     public HttpResponse<?> index() {
-        return HttpResponse.ok(CollectionUtils.mapOf("loggedIn", true, "username", "sdelamo"));
+        log.info("index");
+        return HttpResponse.ok(CollectionUtils.mapOf("loggedIn", false, "username", "sdelamo"));
     }
 }
--- src/main/java/hello/world/ViewsPojoController.java
+++ src/main/java/hello/world/ViewsPojoController.java
@@ -4,12 +4,18 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.views.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/viewspojo")
 class ViewsPojoController {
-    @View("home.html")
-    @Get("/pojo")
-    public HttpResponse<Person> pojo() {
-        return HttpResponse.ok(new Person("sdelamo", true));
+    private static final Logger log
+            = LoggerFactory.getLogger(ViewsPojoController.class);
+
+    @View("home")
+    @Get("/{person}")
+    public HttpResponse<Person> pojo(String person) {
+        log.info("pojo");
+        return HttpResponse.ok(new Person(person, true));
     }
 }
